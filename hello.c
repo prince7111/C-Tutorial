359a88873b190f930e87f98886ab5fe8b0bb9983
prince711 <princemeena7111@gmail.com>
2025-09-05 08:54:29 -0700

    My intro in C ..

--- hello.c
+++ hello.c
@@ -1,5 +1,11 @@
 # include<stdio.h>
 int main(){
-    printf("Hello World");
+   // printf("Hello World");
+   int age=20;
+   
+   printf("the name is prince\n");
+   printf("my age %d",age);
+   printf("\ni live in khirkiya tah,chhipawad mp");
+
     return 0;
 }
