755bd7b1e73d31e8fdde0531c01943e8fafa42f6
prince711 <princemeena7111@gmail.com>
2025-09-05 23:59:22 -0700

    PQ 2: circle arear calcilation

--- chapter1.c
+++ chapter1.c
@@ -1,15 +1,21 @@
 # include <stdio.h>
   int main(){
      //practice Questions 1;
-     //    printf("Q .write a c programe to calculate area of square\n");
-      //    int side =4;
-       //    int areaOfSquar = side * side;
+        //    printf("Q .write a c programe to calculate area of square\n");
+        //    int side =4;
+        //    int areaOfSquar = side * side;
         //    printf("Area of suare :%d",areaOfSquar);
         //Using Scanner class
-        float side;
-        printf("entere side of a suare\n");
-        scanf("%f",&side);
-        printf("Areaa :%f",side*side);
+        // float side;
+        // printf("entere side of a suare\n");
+        // scanf("%f",&side);
+        // printf("Areaa :%f",side*side);
+    printf("practice Question 2;\n");
+    printf("write c program to calculate circle radius is given by user\n");
+                 float r;
+                 printf("enter radius:\n");
+                 scanf("%f",&r);
+                 printf("area of cirecle %f",3.14 *(r*r));
          
 
     
