e5ccc35e88ea1ada2fe12c1d186e03cb56030e3e
prince711 <princemeena7111@gmail.com>
2025-09-05 23:46:06 -0700

    PQ 1,part 2,calculate area using scan f class

--- chapter1.c
+++ chapter1.c
@@ -1,10 +1,16 @@
 # include <stdio.h>
   int main(){
      //practice Questions 1;
-       printf("Q .write a c programe to calculate area of square\n");
-       int side =4;
-       int areaOfSquar = side * side;
-       printf("Area of suare :%d",areaOfSquar);
+     //    printf("Q .write a c programe to calculate area of square\n");
+      //    int side =4;
+       //    int areaOfSquar = side * side;
+        //    printf("Area of suare :%d",areaOfSquar);
+        //Using Scanner class
+        float side;
+        printf("entere side of a suare\n");
+        scanf("%f",&side);
+        printf("Areaa :%f",side*side);
+         
 
     
 
