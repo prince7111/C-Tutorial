1c01f24944e2b9a6e75f09c6972ddf8defff1150
prince711 <princemeena7111@gmail.com>
2025-09-05 23:38:12 -0700

    pQ. 1|calculate area of suare

---
+++ chapter1.c
@@ -0,0 +1,14 @@
+# include <stdio.h>
+  int main(){
+     //practice Questions 1;
+       printf("Q .write a c programe to calculate area of square\n");
+       int side =4;
+       int areaOfSquar = side * side;
+       printf("Area of suare :%d",areaOfSquar);
+
+    
+
+    
+       printf("\ncode runn successfully");
+    return 0;
+}
---
+++ rbc.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main(){
+    printf("hello World \n");
+    printf("C language Rough note book");
+
+    return 0;
+}
