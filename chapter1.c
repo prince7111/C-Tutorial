eb4157789d3dfe8fb6042a63efa21c8a1169e8ff
prince711 <princemeena7111@gmail.com>
2025-09-06 04:51:30 -0700

    multuiply two nuber and use comments

--- chapter1.c
+++ chapter1.c
@@ -1,22 +1,33 @@
 # include <stdio.h>
   int main(){
-     //practice Questions 1;
-        //    printf("Q .write a c programe to calculate area of square\n");
-        //    int side =4;
-        //    int areaOfSquar = side * side;
-        //    printf("Area of suare :%d",areaOfSquar);
-        //Using Scanner class
-        // float side;
-        // printf("entere side of a suare\n");
-        // scanf("%f",&side);
-        // printf("Areaa :%f",side*side);
-    printf("practice Question 2;\n");
-    printf("write c program to calculate circle radius is given by user\n");
-                 float r;
-                 printf("enter radius:\n");
-                 scanf("%f",&r);
-                 printf("area of cirecle %f",3.14 *(r*r));
-         
+   //   practice Questions 1;
+   //         printf("Q .write a c programe to calculate area of square\n");
+   //         int side =4;
+   //         int areaOfSquar = side * side;
+   //         printf("Area of suare :%d",areaOfSquar);
+   //      Using Scanner class
+   //      float side;
+   //      printf("entere side of a suare\n");
+   //      scanf("%f",&side);
+   //      printf("Areaa :%f",side*side);
+
+   // //  printf("practice Question 2;\n");
+   // //  printf("write c program to calculate circle radius is given by user\n");
+   // //               float r;
+   // //               printf("enter radius:\n");
+   // //               scanf("%f",&r);
+   // //               printf("area of cirecle %f",3.14 *(r*r));
+   // /
+   //practice Question 3  multiplyw
+    int a;
+    int b;
+    printf("enter your number a?");
+    scanf("%d",&a);
+    printf("entere yout number b?");
+    scanf("%d",&b);
+    int axb=a*b;
+    printf("Answer:%d",axb);
+
 
     
 
