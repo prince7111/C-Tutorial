20c3aea5050023c4f8356e2a07a45bbed01f330b
prince7111 <princemeena7111@gmail.com>
2025-09-12 22:25:30 +0530

    A Cp to clalculate area of triangle

--- chapter1.c
+++ chapter1.c
@@ -1,40 +1,15 @@
 # include <stdio.h>
   int main(){
-   //   practice Questions 1;
-   //         printf("Q .write a c programe to calculate area of square\n");
-   //         int side =4;
-   //         int areaOfSquar = side * side;
-   //         printf("Area of suare :%d",areaOfSquar);
-   //      Using Scanner class
-   //      float side;
-   //      printf("entere side of a suare\n");
-   //      scanf("%f",&side);
-   //      printf("Areaa :%f",side*side);
-
-   // //  printf("practice Question 2;\n");
-   // //  printf("write c program to calculate circle radius is given by user\n");
-   // //               float r;
-   // //               printf("enter radius:\n");
-   // //               scanf("%f",&r);
-   // //               printf("area of cirecle %f",3.14 *(r*r));
-   // /
-   //practice Question 3  multiplyw
-    int a;// num a
-    int b;//num b
-    printf("enter your number a?");
-    // input a
-    scanf("%d",&a);            
-    printf("entere yout number b?");
-    //input b
-    scanf("%d",&b);
-    //multiply in new variable
-    int axb=a*b;
-    printf("Answer:%d",axb);
+   //write a C programe to calculate area of trianle
+   int a,b;
+   printf("Entere side a ");
+   scanf("%d",&a);
+    printf("Entere side b ");
+   scanf("%d",&b);
+   int area = 1/2*(a*b);
+   printf("area of Triangle is:=%d",&area);
 
 
-    
-
-    
-       printf("\ncode runn successfully");
-    return 0;
-}
+   
+  return 0;
+  }
--- chapter_2.c
+++ chapter_2.c
@@ -1,7 +1,14 @@
 # include <stdio.h>
+#include<math.h>
    ///september 7
 int main(){
-    printf("hello C world");
+ 
+    int a = 4;
+    int  b = 4;
+  int power =pow(a,b);
+  printf("%f",pow);
+
+
 
     return 0;
 }
