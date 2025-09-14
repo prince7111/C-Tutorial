5b3140020a9484b3595d52ef5bc9e003a4c6d8fe
prince7111 <princemeena7111@gmail.com>
2025-09-14 09:09:49 +0530

    chapter two updatation

--- chapter1.c
+++ chapter1.c
@@ -1,15 +1,65 @@
 # include <stdio.h>
   int main(){
-   //write a C programe to calculate area of trianle
-   int a,b;
-   printf("Entere side a ");
-   scanf("%d",&a);
-    printf("Entere side b ");
-   scanf("%d",&b);
-   int area = 1/2*(a*b);
-   printf("area of Triangle is:=%d",&area);
+    //print next line
+    // printf("Hello world\n");
+    // printf("Hello world\n");
+    // printf("Hello world\n");
+    // printf("Hello World\n");
+    //  //
+    //  int age =22;
+    //  float pi = 3.14f;
+    //  char atTherRate = '@';
+    //  printf("age is %d\n",age);
+    //  printf("pi value =%f\n",pi);
+    //  printf("at.-%c\n",atTherRate);
+      
+    // // input in C
+    // int age;
+    // printf("enter your age");
+    // scanf("%d", &age);
+    // printf("%d",age);
+    // add two number A & B
+        // // int a;int b;
+        
+        // // scanf("%d",&a);
+        // // scanf("%d",&b);
+        // // printf("%d",a+b);
+        // //calculate area of square side give by user 
+        // int side;
+        // printf("Enter side of Square");
+        // scanf("%d", &side);
+        // int area = side*side;
+        // printf("area of suare %d\n",area);
+        // printf("Write C program to calculate circle area *radius given by user\n");// circle formula = pi*r^2;
+        // float r;
+        // printf("enter radius of circle");
+        // scanf("%f",&r);
+        // float circleArea = 3.14 *r*r;
+        // printf("Area of Circle:%f",circleArea);
+         // calcultate area of rectangle
+        //  int a;int b;
+        //  scanf("%d",&a);
+        //  scanf("%d",&b);
+        //  int area = a*b;
+        //  printf("area of rctngl %d\n",a*b);
+        // cube
+        // int x;
+        // printf("enter number");
+        // scanf("%d", &x);
+        // printf("cube of given number %d\n",x*x*x);
+        //operation on a & b // basic maths formula
+        // int a,b;
+        // printf("enter a:\n");
+        // scanf("%d",&a);
+        // printf("enter b:\n");
+        // scanf("%d",&b);
+        // printf("add %d\n",a+b);
+        // printf("min %d\n",a-b);
+        // printf("mul %d\n",a*b);
+        // printf("divide %d\n",a/b);
+        printf("CP(C.Prgraming language #PRINCE MEENA 44)");
+        printf("CHAPTER 1 || COMPLEATE SUCCESFUULL");
 
-
-   
+ 
   return 0;
   }
--- chapter_2.c
+++ chapter_2.c
@@ -1,14 +1,16 @@
 # include <stdio.h>
-#include<math.h>
-   ///september 7
-int main(){
- 
-    int a = 4;
-    int  b = 4;
-  int power =pow(a,b);
-  printf("%f",pow);
-
-
+   int main(){
+    printf("hello world\n");
+    // practice Quetsions
+    // true-->1
+    // false-->0
+    //divisible by two
+  //  int n;
+  //  scanf("%d", &n);
+  //  printf("%d",n%2==0);
+  //Q2 odd or even(same code)
+  
+  
 
     return 0;
-}
+   }
