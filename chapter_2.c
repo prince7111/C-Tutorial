e320fd5e734035acaa62161f5e8255cdeb288a4c
prince7111 <princemeena7111@gmail.com>
2025-09-17 12:48:24 +0530

    Q.8b

--- chapter_2.c
+++ chapter_2.c
@@ -2,9 +2,13 @@
    int main(){
    //Practice Q8
    //print out put true or false 
-   int sunday = 1;//true
-   int snowing =1;//true
-   printf("%d",sunday && snowing);
+  //  int sunday = 1;//true
+  //  int snowing =1;//truegc
+  //  printf("%d",sunday && snowing);
+  //b
+  int isMonday =1;
+  int isRaining =0;
+  printf("%d",isMonday || isRaining);
 
     return 0;
    }
