b64a578ad69d729291b5a33d50f7ebdf531c294b
prince7111 <princemeena7111@gmail.com>
2025-09-17 12:53:22 +0530

    check if number is Two digit or not

--- chapter_2.c
+++ chapter_2.c
@@ -6,9 +6,15 @@
   //  int snowing =1;//truegc
   //  printf("%d",sunday && snowing);
   //b
-  int isMonday =1;
-  int isRaining =0;
-  printf("%d",isMonday || isRaining);
+  // int isMonday =1;
+  // int isRaining =0;
+  // printf("%d",isMonday || isRaining);
+  //c
+  // check if number is two digit or not
+  int number;
+  printf("Entere your number:");
+  scanf("%d",&number);
+  printf("%d",(number>9 )&&(number <100));
 
     return 0;
    }
