88e7cf700a3f304fcd1c258e6ccd87bbe0efd7c4
prince7111 <princemeena7111@gmail.com>
2025-12-23 22:48:41 +0530

    data type in c

--- Govt_polytechnic_harda_2025/Computer_Programing_23-12-25.c
+++ Govt_polytechnic_harda_2025/Computer_Programing_23-12-25.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
+//this code dispays how data types work in c
 int main(){
-    printf("Hello world");
+   printf("data types in C");
+   int n = 44;
+    int n2 = 43;
+    char ch = 'p';
+    float dashmlav = 10.10;
+    printf("My favroiute number is:%d",n);
     return 0;
 }
