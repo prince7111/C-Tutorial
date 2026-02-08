a586365e949bfe36f81e1c4556553c217d40a7af
prince7111 <princemeena7111@gmail.com>
2026-02-08 22:19:46 +0530

    Q Area of Circle

--- Chapter_1_Variables.c
+++ Chapter_1_Variables.c
@@ -1,10 +1,18 @@
 #include <stdio.h>
 int main(){
+    //area of circle 
+    float r;
+    printf("enter radius");
+    scanf("%f", &r);
+    printf("area of circle is:%f",3.14 * r * r);
+
+    /*area of square
 int side;
 printf("Enter side ");
 scanf("%d", &side);
 int area = side * side;
 printf("Area of Suare %d",area);
+*/
 
  /*  printf("Hello World");
    int number = 44;
