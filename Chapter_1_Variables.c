4e82fd4b6072cb55731f49654b002c9c0bd4fcc6
prince7111 <princemeena7111@gmail.com>
2026-02-08 22:06:47 +0530

    |1 Varialbles | Q sum of two

--- Chapter_1_Variables.c
+++ Chapter_1_Variables.c
@@ -7,12 +7,21 @@ int main(){
     
     */
    //printf("Hello World");
-   int number = 44;
-   char star = '*';
-   float pi = 3.14;
-   printf("farvari 2025");
-    printf("Prince 44\n");
-   printf("kuch bhi\n");
+//    int number = 44;
+//    char star = '*';
+//    float pi = 3.14;
+//    printf("farvari 2025");
+//     printf("Prince 44\n");
+//    printf("kuch bhi\n");
+int a,b;
+printf("----------");
+printf("enter number a:");
+scanf("%d",&a);
+printf("Entere number b:");
+scanf("%d",&b);
+int sum = a + b;
+printf("Sum=%d\n",sum);
+printf("----------");
 
     return 0;
 }
