9909d697dcdcfe03f97872374431f9a4cdef601d
prince7111 <princemeena7111@gmail.com>
2026-02-08 21:38:28 +0530

    C-Programing | hello world 00

--- Helloworld.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-int main(){
-    printf("Hello World");
-    printf("abhi carrer thik nahi chal rha hai, 18|12 december 2025 \n kya ham vah"
-        "kya ham vah sab kar payegne jo socha hai liffe me, career ki uchiyo me, family ke liye,khud ke liye|\n bohot kuch sahi taike se karne ke jarurat hai samjhdari se sujh bujh se");
-    return 0;
-}
