974a38c2fe430e04ed16972f4fd4f513c7a7709a
prince7111 <princemeena7111@gmail.com>
2026-02-08 21:52:07 +0530

    |chapter 1 | Variables |

---
+++ Chapter_1_Variables.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main(){
+
+    /*Variables
+    
+    
+    
+    */
+   //printf("Hello World");
+   int number = 44;
+   char star = '*';
+   float pi = 3.14;
+   printf("farvari 2025");
+    printf("Prince 44\n");
+   printf("kuch bhi\n");
+
+    return 0;
+}
