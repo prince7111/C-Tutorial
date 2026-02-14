71f6e4f2f16dd5f334d3360f78d3cf090337789d
prince7111 <princemeena7111@gmail.com>
2026-02-14 18:03:52 +0530

    Practice Q1

--- hello.c
+++ hello.c
@@ -1,5 +1,12 @@
 # include <stdio.h>
-int main(){
-    printf("Hello World");
+  int main(){
+    /* Prctice
+    Q1 WRITE A PROGRAM TO CALCULATE AREA OF SQUARE 
+    */
+    int SIDE;
+    printf("Entere side :");
+    scanf("%d",&SIDE);
+    printf("Araa of Suqare is:%d",SIDE * SIDE);
+
     return 0;
-}
+  }
