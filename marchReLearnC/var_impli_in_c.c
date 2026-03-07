41a983070bfe012afeb7df8ba935be1f2552a3a3
prince7111 <princemeena7111@gmail.com>
2026-03-07 11:49:16 +0530

    C: Variables in C

---
+++ marchReLearnC/var_impli_in_c.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+    /*VARIABLES : A variable in c is a named storage location
+    int the computer memmory used to hold data tha can be ac-
+    cessed and modified throughout the programsi execution.
+    */
+
+    int number = 25;
+    char star = '*';
+   // printf("%c",star);//variables
+   int a = 30;
+   int A = 40;
+   int _age = 22;
+ 
+  
+    return 0;
+}
