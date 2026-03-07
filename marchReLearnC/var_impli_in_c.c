5c0086820cc6f737a5f224202138ed5f68345bf4
prince7111 <princemeena7111@gmail.com>
2026-03-07 11:57:12 +0530

    C:Data Type

--- marchReLearnC/var_impli_in_c.c
+++ marchReLearnC/var_impli_in_c.c
@@ -1,17 +1,11 @@
 #include<stdio.h>
 int main(){
-    /*VARIABLES : A variable in c is a named storage location
-    int the computer memmory used to hold data tha can be ac-
-    cessed and modified throughout the programsi execution.
-    */
-
-    int number = 25;
-    char star = '*';
-   // printf("%c",star);//variables
-   int a = 30;
-   int A = 40;
-   int _age = 22;
- 
-  
+  //DATA tyepe
+  int age = 20;
+  float pi = 3.14;
+  char ch = 'A';
+  printf("%d\n",age);
+  printf("%f\n",pi);
+  printf("%c\n",ch);
     return 0;
 }
