450250776651f2d298b33e7b201cbff1932b8d4d
prince7111 <princemeena7111@gmail.com>
2026-03-07 20:38:05 +0530

    sizeof() function/Opearator

---
+++ marchReLearnC/sizeoperator.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(){
+    printf("Size of int: %zu bytes\n",sizeof(int));
+    printf("size of double: %zu bytes\n",sizeof(double));
+    printf("Size of float: %zu bytes\n",sizeof(float));
+    printf("size of char: %zu bytes\n",sizeof(char));
+    return 0;
+}
--- marchReLearnC/var_impli_in_c.c
+++ marchReLearnC/var_impli_in_c.c
@@ -1,11 +1,12 @@
-#include<stdio.h>
+#include <stdio.h>
 int main(){
-  //DATA tyepe
   int age = 20;
   float pi = 3.14;
-  char ch = 'A';
-  printf("%d\n",age);
-  printf("%f\n",pi);
-  printf("%c\n",ch);
-    return 0;
+  char star = '*';
+  //Output*
+  printf()
+
+
+
+  return 0;
 }
