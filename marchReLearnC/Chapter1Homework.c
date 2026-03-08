276b4746227aed7f079d2e53c1ab9a4f88e57dc4
prince7111 <princemeena7111@gmail.com>
2026-03-08 14:58:45 +0530

    cube of n

--- marchReLearnC/Chapter1Homework.c
+++ marchReLearnC/Chapter1Homework.c
@@ -1,8 +1,20 @@
 #include <stdio.h>
 int main(){
-     //cd 
-     int a= 4;
+    /*
+    //chapter 1 ; HomeWork
+     //Q1 whrite a program to calclater perimeter of rectangle 
+    int a= 4;
      int b= 5;
      int perOfRectangle = 2 *(a + b);
      printf("perimeter of rectangle is :%d",perOfRectangle);
+
+     //Q2 take a numner n from user & output its cube (n*n*n)
+     */
+     int n ;
+     printf("Enter your number ");
+     scanf("%d",& n);
+     printf("cube of a given number is = %d",(n*n*n));
+     return 0;
+
+
 }
