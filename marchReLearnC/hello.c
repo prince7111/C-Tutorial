b3bbab01c961f1f72dd7e23bb0264c5f5d0599ee
prince7111 <princemeena7111@gmail.com>
2026-03-08 14:13:48 +0530

    re practice c

--- marchReLearnC/hello.c
+++ marchReLearnC/hello.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 int main(){
-    printf("Hello World");
+    printf("Hello World || 08-03-26");
+    printf("\n");
+    int age = 21;
+    float pi = 3.14;
+    char symbol = '$';
+    printf("print Symbol:%c\n",symbol);
+    printf("age is:%d\n",age);
+    printf("pi value:%f\n",pi);
+
     return 0;
 }
