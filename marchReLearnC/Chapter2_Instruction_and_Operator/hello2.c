d990bef51b610d1af66d28dd73f8c573101f914b
prince7111 <princemeena7111@gmail.com>
2026-03-09 15:44:23 +0530

    C Chapter 2 :Instructions and Operators

--- marchReLearnC/Chapter2_Instruction_and_Operator/hello2.c
+++ marchReLearnC/Chapter2_Instruction_and_Operator/hello2.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 int main(){
-    printf("Hello World");
+    printf("Hello World\n");
+   int a,b;
+   a = b = 4;
+   printf("the value of a %d  the value of b %d",a,b);
     
     return 0;
 }
