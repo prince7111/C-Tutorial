267bf23303a12e5664bf8c9fb1f8cdd6cbb5fd1f
prince7111 <princemeena7111@gmail.com>
2026-03-09 16:27:52 +0530

    isDivisibleBy2

--- marchReLearnC/Chapter2_Instruction_and_Operator/hello2.c
+++ marchReLearnC/Chapter2_Instruction_and_Operator/hello2.c
@@ -1,9 +1,7 @@
 #include <stdio.h>
 int main(){
-    printf("Hello World\n");
-   int a,b;
-   a = b = 4;
-   printf("the value of a %d  the value of b %d",a,b);
+  int a = 1.99999;
+  printf("%d",a);
     
     return 0;
 }
