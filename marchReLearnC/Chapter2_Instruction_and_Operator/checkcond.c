7e2aab23831b44beb2523f0ed1f213039541d998
prince7111 <princemeena7111@gmail.com>
2026-03-09 19:30:50 +0530

    check (c) two digit number

--- marchReLearnC/Chapter2_Instruction_and_Operator/checkcond.c
+++ marchReLearnC/Chapter2_Instruction_and_Operator/checkcond.c
@@ -6,9 +6,17 @@ int main(){
 //    int ans = isSnowing & isSunday;
 //    printf("%d",ans);
    //(b)
-   int isMonday = 1;
-   int isRaining = 0;
-   int ans = isMonday || isRaining;
-   printf("%d",ans);
+//    int isMonday = 1;
+//    int isRaining = 0;
+//    int ans = isMonday || isRaining;
+//    printf("%d",ans);
+//(c)
+int n =10;
+int ans = ((n<100) && (n>9));
+if(ans == 0){
+    printf("not a two digit number");
+}else{
+    printf(" a two  digit number"); 
+}
     return 0;
 }
