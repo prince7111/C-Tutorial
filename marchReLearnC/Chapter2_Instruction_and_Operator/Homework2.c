bbbc50b0cac5966df595c8e9cea870c302bec2a3
prince7111 <princemeena7111@gmail.com>
2026-03-09 19:52:42 +0530

    check smallest number in three

--- marchReLearnC/Chapter2_Instruction_and_Operator/Homework2.c
+++ marchReLearnC/Chapter2_Instruction_and_Operator/Homework2.c
@@ -7,13 +7,27 @@ int main(){
     // printf("%d",avg);
 
   
-    char character ;
-    scanf("%c",&character);
-    if((character >=48) && (character <=57)){
-    printf("its digit");
-    }else{
-        printf("its not  digit");
+    // char character ;
+    // scanf("%c",&character);
+    // if((character >=48) && (character <=57)){
+    // printf("its digit");
+    // }else{
+    //     printf("its not  digit");
 
+    // }
+    int a,b,c;
+    printf("enter your number a");
+    scanf("%d",&a);
+    printf("enter your number b");
+    scanf("%d",&b);
+    printf("enter your number c");
+    scanf("%d",&c);
+    if(a>b && a >c){
+        printf("a is grater");
+    }else if(b>a && b>c){
+        printf("b is greater");
+    }else{
+        printf("c is greater");
     }
     return 0;
 }
