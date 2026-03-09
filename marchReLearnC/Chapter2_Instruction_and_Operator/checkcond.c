180f9a6d7f437873336e8f5638ee42b3ab9cc0b3
prince7111 <princemeena7111@gmail.com>
2026-03-09 19:24:07 +0530

    B check conditon

--- marchReLearnC/Chapter2_Instruction_and_Operator/checkcond.c
+++ marchReLearnC/Chapter2_Instruction_and_Operator/checkcond.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 int main(){
-   int isSunday = 1;
-   int isSnowing = 0;
-   int ans = isSnowing & isSunday;
+    //@
+//    int isSunday = 1;
+//    int isSnowing = 0;
+//    int ans = isSnowing & isSunday;
+//    printf("%d",ans);
+   //(b)
+   int isMonday = 1;
+   int isRaining = 0;
+   int ans = isMonday || isRaining;
    printf("%d",ans);
     return 0;
 }
