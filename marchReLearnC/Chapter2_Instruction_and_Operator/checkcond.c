182ce1889f1953edeb5dab30ca864fe7ac5bc7c0
prince7111 <princemeena7111@gmail.com>
2026-03-09 19:19:41 +0530

    check if codniton is valid our invalid true(1) & false (0)

--- marchReLearnC/Chapter2_Instruction_and_Operator/checkcond.c
+++ marchReLearnC/Chapter2_Instruction_and_Operator/checkcond.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main(){
    int isSunday = 1;
-   int isSnowing = 1;
+   int isSnowing = 0;
    int ans = isSnowing & isSunday;
    printf("%d",ans);
     return 0;
--- marchReLearnC/Chapter2_Instruction_and_Operator/isDevideBy2.c
+++ marchReLearnC/Chapter2_Instruction_and_Operator/isDevideBy2.c
@@ -1,14 +1,6 @@
 #include<stdio.h>
 #include<math.h>
 int main(){
-    int n;
-    printf("Entere your number:");
-    scanf("%d",&n);
-    if(n % 2 ==0){
-        printf("divisible");
-    }else{
-        printf("Undivisible");
-    }
-    
+  
     return 0;
 }
