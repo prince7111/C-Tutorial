43c105659a5bd350403895e136e70806ec16b0f9
prince7111 <princemeena7111@gmail.com>
2026-03-09 12:47:52 +0530

    C pro abhyaas -9-march-2026 lesson 1

--- marchReLearnC/hello.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main(){
-   int age;
-   printf("Entere your age\n:");
-   scanf("%d",& age);
-   printf("age is:%d",age);
-
-    return 0;
-}
---
+++ marchReLearnC/hello1.c
@@ -0,0 +1,46 @@
+//C Variables datatypes + input output
+#include <stdio.h>
+int main(){
+    // printf("Hello world");
+    // // float a;
+    // // float b;
+    // // printf("Enter your number :");
+    // // scanf("%f",&a);
+    // // printf("Enter your number :");
+    // // scanf("%f",&b);
+    // // float sum = a + b;
+    // // printf("%f",sum);
+    // //{Area of sqaue = side^2}
+    // printf("Prince Meena C Programing \n");
+    // // int side = 4;
+    // // printf("area of given side of square is :%d",side * side);
+    // printf("C programing language \n");
+    // // int radius; //r of circle
+    // // printf("Enter r of any circle you want to calculates its area:");
+    // // scanf("%d",&radius);
+    // // printf("area of given r of circle is:%f",(3.14)*(radius*radius));
+    //  printf(".....\n");
+    // // int lenght = 26;
+    // // int breadth = 34;
+    // // int perimeter = 2 * ( lenght + breadth ) ;
+    // // printf("%d",perimeter);
+    //
+    // int n;
+    // printf("Entere your num:\n");
+    // scanf("%d",&n);
+    // int cube = n * n * n ;
+    // printf("%d cube = %d",n ,cube);
+    //
+      //program a+b;
+      int a = 4;
+      int b = 3;
+      //add a & b
+      int sum = a + b;
+      //print output
+      printf("a + b =%d",sum);
+
+
+
+
+    return 0;
+}
