059a42c855fef25877cc555100dab27e3bfd01fe
prince7111 <princemeena7111@gmail.com>
2026-03-11 10:20:19 +0530

    C practice 11 - 03 - 2026

--- marchReLearnC/Chapter3/checkAdult.c
+++ marchReLearnC/Chapter3/checkAdult.c
@@ -1,12 +1,5 @@
 #include<stdio.h>
 int main(){
-    int age;
-    printf("Enter your age:");
-    scanf("%d",&age);
-    if(age > 18 ){
-        printf("adult");
-    }else{
-        printf("not adult");
-    }
+  
 
 }
