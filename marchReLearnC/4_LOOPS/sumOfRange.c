26ad66b9cb509011c0f197896e7c0603a2ed8389
prince7111 <princemeena7111@gmail.com>
2026-03-11 15:38:26 +0530

    sum of range

---
+++ marchReLearnC/4_LOOPS/sumOfRange.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    int n1 = 0;
+    int n2 = 200;
+    int sum =0;
+    for(int i = 1;i<199; i++){
+        sum = sum + i;
+        
+    }
+    printf("%d",sum);
+    return 0;
+}
