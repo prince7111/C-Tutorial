12d164360901d2a185563c1765a6d84550f0f19f
prince7111 <princemeena7111@gmail.com>
2026-03-12 12:03:34 +0530

    add two numbers

---
+++ marchReLearnC/Chapter5/addFunction.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int add(int a,int b);
+int main(){
+    int a,b;
+    
+    
+    printf("sum of two number is %d",add(33,37));
+    
+
+
+
+
+
+    return 0;
+}
+int add (int a,int b){
+    return a + b;
+}
