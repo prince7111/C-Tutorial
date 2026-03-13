6b2ed8a00d0cd4d33853441c97f2456d66260df8
prince7111 <princemeena7111@gmail.com>
2026-03-13 17:15:10 +0530

    age = 22; *ptr = &age

--- marchReLearnC/Chapter6/p.c
+++ marchReLearnC/Chapter6/p.c
@@ -2,9 +2,11 @@
 int main(){
      int age = 22;
      int *ptr = &age;
-     printf("age = %d",age);
-     printf("\n");
-     printf("*ptr = %d",*ptr);
+     printf("print value of age:%d\n",age); // 22
+      printf("print ptr stored add:%d\n",ptr);//....
+     printf("print ptr stored add:%p\n",ptr); //ffffhh
+     printf("print *ptr value at add: %d\n",*ptr);//22
+
      
     return 0;
 }
