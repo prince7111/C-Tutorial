7d8058598edf9cd4a737d4f170c01100db50951e
prince7111 <princemeena7111@gmail.com>
2026-03-14 11:03:40 +0530

    val pringing in poinert

--- marchReLearnC/Chapter6/hello6.c
+++ marchReLearnC/Chapter6/hello6.c
@@ -1,13 +1,11 @@
 #include<stdio.h>
 int main(){
-    int n = 45;
-
-    int *ptr = &n;
-    printf("Value of x= %d\n",n); //45;
-    printf("Address of x %d\n",&n);
-    printf("Address of x %p\n", &n);// 7ff
-     printf("Address of x %u\n", &n);// 7ff
-    printf("value of pointer= %d",*ptr);
-
+   int age = 22;
+   int *ptr = &age;
+   printf("&age =%d\n",&age);
+   printf("&*ptr= %d\n",&*ptr);
+   printf("&ptr =%d\n",&ptr);
+   printf("age %d ",age);
+   printf("*ptr %d",*ptr);
     return 0;
 }
--- marchReLearnC/Chapter6/p.c
+++ marchReLearnC/Chapter6/p.c
@@ -1,11 +1,9 @@
 #include<stdio.h>
 int main(){
-     int age = 22;
-     int *ptr = &age;
-     printf("print value of age:%d\n",age); // 22
-      printf("print ptr stored add:%d\n",ptr);//....
-     printf("print ptr stored add:%p\n",ptr); //ffffhh
-     printf("print *ptr value at add: %d\n",*ptr);//22
+    int a = 22;
+    int *ptr = &a;
+    printf("%d",*ptr);
+    
 
      
     return 0;
