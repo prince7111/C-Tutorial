6c3529e16e83641d01d0e3a55328dbc7ff557a12
prince7111 <princemeena7111@gmail.com>
2026-03-19 18:11:34 +0530

    gstarray | direct initializeing arrya

--- marchReLearnC/7-Array/gstarray.c
+++ marchReLearnC/7-Array/gstarray.c
@@ -2,15 +2,8 @@
 //write a program to enter price of 3 prints their final code with gst
 
 int main(){
-    float price[3];
-    printf("enter price\n");
-    scanf("%f",&price[0]);
-
-    printf("enter price\n");
-    scanf("%f",&price[1]);
-
-    printf("enter price\n");
-    scanf("%f",&price[2]);
+    float price [] = {44,44,44};
+    
 
     printf("total price 1: %f",price[0] + (0.18  *(price[0])));
     printf("total price 1: %f",price[1] + (0.18  *(price[1])));
