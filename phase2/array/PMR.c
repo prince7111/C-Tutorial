3732eb4dc37e361e41de3d1df1beea2ebca76b6e
prince7111 <princemeena7111@gmail.com>
2026-03-19 12:24:10 +0530

    printLetters

--- phase2/array/PMR.c
+++ phase2/array/PMR.c
@@ -1,21 +1,16 @@
 #include<stdio.h>
-void pR(float array[],int n);
+void printLetter(char *ptr);
 int main(){
-  float array[] = {1.2,2.3,3.4,4.5,5.5};
-  int n = 5;
-  pR(array,n);
-  for(int i = 0; i<n;i++){
-    printf("%f,",array[i]);
-  }
-  return 0;
-}
-void pR(float array[],int n){
-  for(int i = 0; i<= 2;i++){
-    float temp = array[i];
-    array[i] = array[n-i-1];
-    array[n-i-1] = temp;
+  char ch = 'A';
+
+  char *ptr = &ch;
+  for(int i = 0; i<26; i++){
+    printf("%c,",(*ptr) + i);
   }
   
-
+  
+  return 0;
 }
+
+
 
