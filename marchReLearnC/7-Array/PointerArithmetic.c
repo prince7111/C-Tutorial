f9788d111bb4f342dcfd70dfc6f2c71c56a0f5a3
prince7111 <princemeena7111@gmail.com>
2026-03-19 18:33:43 +0530

    pointer Arithmetic

---
+++ marchReLearnC/7-Array/PointerArithmetic.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    printf("Pointer Arithmetic\n");
+    int age = 20;
+    int *ptr = &age;
+    printf("%u\n",ptr);
+    ptr ++;
+    printf("%u\n",ptr);
+    return 0;
+
+
+    return 0;
+}
