f4dc70ea6ad494053a94518de40d322ed57e0eb1
prince7111 <princemeena7111@gmail.com>
2026-03-19 15:32:44 +0530

    commit lesson 2:

---
+++ marchReLearnC/2-Insn-Operator/hello2.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(){
+   int isSunday = 1;
+   int isSnowing = 1;
+   int ans = isSnowing & isSunday;
+   printf("%d",ans);
+    return 0;
+}
---
+++ marchReLearnC/2-Insn-Operator/modularOperator.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main(){
+    printf("%d\n",-10 % 4);
+    printf("%d\n",10 % 4);
+
+    return 0;
+}
