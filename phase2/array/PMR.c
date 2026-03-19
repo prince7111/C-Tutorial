07ef90430d0092ea7145bc64b481a659c3e8038d
prince7111 <princemeena7111@gmail.com>
2026-03-19 09:20:57 +0530

    pointer f.Specfier

--- phase2/array/PMR.c
+++ phase2/array/PMR.c
@@ -1,9 +1,10 @@
 #include<stdio.h>
 int main(){
+    
+
     int age = 22;
     int *ptr = &age;
-    int _age = *ptr;
-    printf("value:%d\n",_age);//22
+    printf("%u",&age);
  
     return 0;
 }
