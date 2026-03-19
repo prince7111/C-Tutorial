d02c638a7664943c83c289d885c22a7549dcfd07
prince7111 <princemeena7111@gmail.com>
2026-03-19 09:30:05 +0530

    print pointer val

--- phase2/array/PMR.c
+++ phase2/array/PMR.c
@@ -1,10 +1,13 @@
 #include<stdio.h>
 int main(){
-    
-
-    int age = 22;
+    int age = 20;
     int *ptr = &age;
-    printf("%u",&age);
- 
+    printf("%d",age);
+    printf("\n");
+    printf("%d",*ptr);
+    printf("\n");
+    printf("%d",*(&age));
+    printf("\n");
+
     return 0;
 }
