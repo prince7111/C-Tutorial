2c6101c37b6ea51134fc532d6b1b7e45597e39e9
prince7111 <princemeena7111@gmail.com>
2026-03-19 11:55:55 +0530

    pointer Question maximum number:

--- phase2/array/PMR.c
+++ phase2/array/PMR.c
@@ -1,24 +1,15 @@
 #include<stdio.h>
 int main(){
-   int *ptr;
-   int x;
-   ptr = &x;
-   *ptr = 4;
+  int a = 2323;
+  int b = 4738;
+  int *ptr = &a;
+  int *ptr2 = &b ;
+  if(*ptr > *ptr2){
+    printf("%d is maiximum",a);
+  }else{
+    printf("%d is maiximum",b);
 
-   printf("\n%d",*ptr);//4
-   printf("\n%d",x);//4
-
-   *ptr += 5;
-   printf("\n%d",*ptr); //9
-   printf("\n%d",x);//9
-   (*ptr)++;
-
-   printf("\n%d",*ptr);//10
-   printf("\n%d",x);//10
-
-
-
-
-
-    return 0;
+  }
+  return 0;
 }
+
