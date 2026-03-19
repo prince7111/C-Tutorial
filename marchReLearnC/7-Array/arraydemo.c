a5b05ad87a6a2c0b37d21f6509c70f667051c967
prince7111 <princemeena7111@gmail.com>
2026-03-19 15:29:33 +0530

    Array in c

---
+++ marchReLearnC/7-Array/arraydemo.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    //int maths = 32;
+    //int phy  = 75;
+    //int chem = 69;
+
+    int arr[] = {32, 75,69};
+    printf("print chem marks:%d\n",arr[2]);
+    printf("print maths marks:%d\n",arr[0]);
+    printf("print phy marks:%d\n",arr[1]);
+
+    return 0;
+}
