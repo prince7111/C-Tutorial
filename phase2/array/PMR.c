8289fa4e2f5d14c831ba46980d6fd403df8ae9a9
prince7111 <princemeena7111@gmail.com>
2026-03-19 09:39:57 +0530

    pointer Quesions'

--- phase2/array/PMR.c
+++ phase2/array/PMR.c
@@ -1,13 +1,24 @@
 #include<stdio.h>
 int main(){
-    int age = 20;
-    int *ptr = &age;
-    printf("%d",age);
-    printf("\n");
-    printf("%d",*ptr);
-    printf("\n");
-    printf("%d",*(&age));
-    printf("\n");
+   int *ptr;
+   int x;
+   ptr = &x;
+   *ptr = 4;
+
+   printf("\n%d",*ptr);//4
+   printf("\n%d",x);//4
+
+   *ptr += 5;
+   printf("\n%d",*ptr); //9
+   printf("\n%d",x);//9
+   (*ptr)++;
+
+   printf("\n%d",*ptr);//10
+   printf("\n%d",x);//10
+
+
+
+
 
     return 0;
 }
