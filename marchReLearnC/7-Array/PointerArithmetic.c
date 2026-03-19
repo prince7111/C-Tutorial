eebc95166270ebf0158a9621be988ede2e42be8e
prince7111 <princemeena7111@gmail.com>
2026-03-19 18:50:24 +0530

    pointerArithmetic float

--- marchReLearnC/7-Array/PointerArithmetic.c
+++ marchReLearnC/7-Array/PointerArithmetic.c
@@ -2,11 +2,16 @@
 int main(){
     printf("Pointer Arithmetic\n");
     int age = 20;
-    int *ptr = &age;
+   // int *ptr = &age;
+
+    float price = 20.00;
+    float *ptr = &price;
     printf("%u\n",ptr);
-    ptr ++;
+    ptr++;
     printf("%u\n",ptr);
-    return 0;
+    ptr--;
+    printf("%u\n",ptr);
+    
 
 
     return 0;
