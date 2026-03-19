5b313f69c0e0674c142a30324a856c0ce71fe020
prince7111 <princemeena7111@gmail.com>
2026-03-19 19:55:33 +0530

    pointer arithmetic

--- marchReLearnC/7-Array/PointerArithmetic.c
+++ marchReLearnC/7-Array/PointerArithmetic.c
@@ -1,7 +1,14 @@
 #include<stdio.h>
 int main(){
     printf("Pointer Arithmetic\n");
-    int age = 20;
+    int age = 23;
+    int _age = 23;
+    int *ptr = &age;
+    int *_ptr = &_age;
+
+    printf("Diff:%u\n",ptr - _ptr);
+    printf("Comparision:%u\n", (*ptr ==*_ptr));
+    //////// int age = 20;
    // int *ptr = &age;
 
     // float price = 20.00;
@@ -13,13 +20,14 @@ int main(){
     // printf("%u\n",ptr);
 
     //case 3
-    char star = '*';
-    char *ptr = &star;
-    printf("%u\n",ptr);
-    ptr--;
-    printf("%u\n",ptr);
-    ptr++;
-    printf("%u\n",ptr);
+
+    // char star = '*';
+    // char *ptr = &star;
+    // printf("%u\n",ptr);
+    // ptr--;
+    // printf("%u\n",ptr);
+    // ptr++;
+    // printf("%u\n",ptr);
 
     
 
