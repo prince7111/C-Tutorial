545ec3f8dfe75aefac948ac19b6571de572727df
prince7111 <princemeena7111@gmail.com>
2026-03-19 12:05:39 +0530

    Q2.

--- phase2/array/PMR.c
+++ phase2/array/PMR.c
@@ -1,15 +1,21 @@
 #include<stdio.h>
+void pR(float array[],int n);
 int main(){
-  int a = 2323;
-  int b = 4738;
-  int *ptr = &a;
-  int *ptr2 = &b ;
-  if(*ptr > *ptr2){
-    printf("%d is maiximum",a);
-  }else{
-    printf("%d is maiximum",b);
-
+  float array[] = {1.2,2.3,3.4,4.5,5.5};
+  int n = 5;
+  pR(array,n);
+  for(int i = 0; i<n;i++){
+    printf("%f,",array[i]);
   }
   return 0;
+}
+void pR(float array[],int n){
+  for(int i = 0; i<= 2;i++){
+    float temp = array[i];
+    array[i] = array[n-i-1];
+    array[n-i-1] = temp;
+  }
+  
+
 }
 
