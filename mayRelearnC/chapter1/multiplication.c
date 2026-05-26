ee44b27cba3473e32bdb182eed8c533c2f1bb9b5
prince7111 <princemeena7111@gmail.com>
2026-05-26 14:54:36 +0530

    comments on A*B Program

---
+++ mayRelearnC/chapter1/multiplication.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+    //Declaration
+    int a;
+    int b;
+    //input valur & initializing in variable
+    printf("Enter a:");
+    scanf("%d",&a);
+    printf("Enter b:");
+    scanf("%d",&b);
+    //multiply
+    int mul = a * b;
+    printf("OUTPUT:%d",mul);
+
+
+    return 0;
+}
