504f9285eb032880fde6d09d6b239456a1a5269a
prince7111 <princemeena7111@gmail.com>
2026-05-26 13:08:29 +0530

    1) Calc Area Of Sqr

---
+++ mayRelearnC/chapter1/AreaOfSquare.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    printf("Calculate area of Square\n");
+        printf("Enter side:\n");
+
+    int side;
+    scanf("%d",& side);
+    printf("Ans = %d",side * side);
+
+    return 0;
+}
