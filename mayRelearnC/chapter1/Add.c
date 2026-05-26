8f39079d5cf74080c2b5e6b2ffa21c0253614e9d
prince7111 <princemeena7111@gmail.com>
2026-05-26 14:28:28 +0530

    sum of two number

---
+++ mayRelearnC/chapter1/Add.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+    int a,b;
+    printf("Enter your number a:");
+    scanf("%d",&a);
+    printf("Eneter your number b:");
+    scanf("%d",&b);
+    printf("Sum of a & b = %d",a+b);
+    return 0;
+}
